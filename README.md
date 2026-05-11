d9d51a3430ff7c21d4734077b57a260e273b1a10
jonathanrundjan <jonathan.yitskhaq@ui.ac.id>
2026-05-11 21:43:32 +0700

    Initial subscriber code.

---
+++ README.md
@@ -0,0 +1,1 @@
+# advrog-module9-subscriber
---
+++ tutorial8/subscriber/src/main.rs
@@ -0,0 +1,48 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+use std::time;
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn get_handler_action(&self) -> String {
+        "user_created".to_owned()
+    }
+
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        let _ten_millis = time::Duration::from_millis(1000);
+        let _now = time::Instant::now();
+
+        // thread::sleep(_ten_millis);
+
+        println!(
+            "In Rafa's Computer [2406344675]. Message received: {:?}",
+            message
+        );
+
+        Ok(())
+    }
+}
+
+fn main() {
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
+}
